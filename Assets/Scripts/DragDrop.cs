c6e9229f516df1b8544c27f6652c17eb8bcf7ad7
radaken <alexon249@gmail.com>
2022-04-14 17:25:24 +0300

    Make child/parent connection by var

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -4,27 +4,37 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler{
     [SerializeField] private Canvas canvas;
-    [SerializeField] GameObject DINAttached;
+
+    public GameObject myParent;
+
     private RectTransform rectTransform;
     private CanvasGroup canvasGroup;
+
     private void Awake(){
         rectTransform = GetComponent<RectTransform>();
         canvasGroup = GetComponent<CanvasGroup>();
     }
+
    public void OnPointerDown(PointerEventData eventData) {
    }
+
    public void OnBeginDrag(PointerEventData eventData) {
-        //eventdata.pointerdrag.transform.setparent(canvasgroup.transform);
-        //if (itemslot.objectattached != null)
-        //{
-        //    itemslot.objectattached = null;
-        //}
+
+        if (this.myParent != null)
+        {
+            myParent.GetComponent<ItemSlot>().myChild = null;
+            myParent = null;
+        }
+
         canvasGroup.alpha = .6f;
-   }
+        canvasGroup.blocksRaycasts = false;
+    }
+
    public void OnEndDrag(PointerEventData eventData) {
        canvasGroup.blocksRaycasts = true;
        canvasGroup.alpha = 1f;
    }
+
    public void OnDrag(PointerEventData eventData) {
        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
    }
